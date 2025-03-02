f3dc8056454b0e2dd0adc0adac564ced72ba6427
GodBlessedHans <haseveco@gmail.com>
2025-03-02 18:10:49 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Broma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.Location = new System.Drawing.Point(188, 139);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(408, 24);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "¿Realmente desea eliminar su disco duro?";
+            // 
+            // btnSi
+            // 
+            this.btnSi.Location = new System.Drawing.Point(241, 243);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(100, 45);
+            this.btnSi.TabIndex = 1;
+            this.btnSi.Text = "Si";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnSi_MouseClick);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(448, 243);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(103, 45);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnNo_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Broma
+{
+    public partial class Form1 : Form
+    {
+        // Random para generar números aleatorios
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNo_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Obtener las dimensiones del formulario
+            int formWidth = this.ClientSize.Width;
+            int formHeight = this.ClientSize.Height;
+
+            // Obtener las dimensiones del botón
+            int buttonWidth = btnNo.Width;
+            int buttonHeight = btnNo.Height;
+
+            // Generar coordenadas aleatorias dentro de los límites del formulario
+            int newX = random.Next(0, formWidth - buttonWidth);
+            int newY = random.Next(0, formHeight - buttonHeight);
+
+            // Asignar la nueva ubicación al botón
+            btnNo.Location = new Point(newX, newY);
+        }
+
+        private void btnSi_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Disco duro eliminado");
+        }
+    }
+}
